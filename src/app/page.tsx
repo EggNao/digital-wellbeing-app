b37af49f8b3a2e227bf363958205481d6bfa2528
naoki eguchi <89183913+EggNao@users.noreply.github.com>
2023-11-20 14:05:27 +0900

    とりあえず表示させる情報を列挙

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,71 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import * as React from "react";
+import { Box, Button, Modal, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box sx={{ height: "100vh" }}>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: { xs: "90%", md: "50%" },
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 2,
+          }}
+        >
+          <Box
+            sx={{
+              textAlign: "center", // タイトルを中央に配置
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              ゲストWi-Fiへようこそ
+            </Typography>
+          </Box>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: "red", fontweight: "bold" }}
+          >
+            このWI-Fiはこのデバイスの通信速度を低下させることができます。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            動画ストリーミングなどの通信量を多く消費するコンテンツの利用を控えましょう。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            通信量を多く消費するコンテンツの使用を控えると周囲の人のインターネット利用体験が向上します。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            通信量を多く消費するコンテンツの利用を控えてください。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            この30分であなたはYoutubeを20分見ていることを検出しました。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            あなたの通信速度を制限することもできますが，ここで通信量を多く消費するコンテンツの利用を控えると，引き続き快適にネットワークを利用できます。
+          </Typography>
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Button variant="contained" onClick={handleClose}>
+              Close
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
 }
